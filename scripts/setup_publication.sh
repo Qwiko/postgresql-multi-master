21abe6ae20cd51884c291074efc423b8544cf774
Jakob Mellberg <jakob.mellberg@gmail.com>
2026-03-15 19:53:36 +0100

    Cleanup

--- scripts/setup_publication.sh
+++ scripts/setup_publication.sh
@@ -4,8 +4,8 @@ set -e
 DB_NAME=${1:-"db"}
 DB_USER=${2:-"admin"}
 
-# Create the Local publication
-psql -v ON_ERROR_STOP=1 -U "$DB_USER" -d $DB_NAME <<-EOSQL
+# Create the local publication
+psql -U "$DB_USER" -d $DB_NAME <<-EOSQL
     CREATE PUBLICATION publication FOR ALL TABLES;
 EOSQL
 echo "Publication publication created."
